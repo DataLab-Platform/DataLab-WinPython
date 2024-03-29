b0ec54c58408539ace0edb3bc30bf871e9bde326
Pierre Raybaut <p.raybaut@codra.fr>
2024-03-29 15:34:09 +0100

    Upgrading `pip` before downloading packages

--- scripts/download.bat
+++ scripts/download.bat
@@ -11,5 +11,6 @@ if not exist "%CI_WNM%" ( "C:\Program Files\7-Zip\7z.exe" x -y -o"." "%CI_WPI%" 
 
 call "%CI_WNM%\scripts\env_for_icons.bat" %*
 
+%PYTHON% -m pip install --upgrade pip
 pip download cdl==%CI_VER% -d packages
 pip download -r requirements.txt -d packages
