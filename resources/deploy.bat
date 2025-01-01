18eb307927f98b248099fb176f973d284bc7f0d6
Pierre Raybaut <pierre.raybaut@gmail.com>
2025-01-01 18:50:33 +0100

    Fix image magick conversion commands

--- resources/deploy.bat
+++ resources/deploy.bat
@@ -2,10 +2,10 @@
 
 set INKSCAPE_PATH="C:\Program Files\Inkscape\bin\inkscape.exe"
 
-@REM Generating images for NSIS installer
+@REM Generating images for Wix installer
 %INKSCAPE_PATH% "WixUIBanner.svg" -o "temp.png" -w 493 -h 58
-magick convert "temp.png" bmp3:"banner.bmp"
+magick "temp.png" bmp3:"banner.bmp"
 %INKSCAPE_PATH% "WixUIDialog.svg" -o "temp.png" -w 493 -h 312
-magick convert "temp.png" bmp3:"dialog.bmp"
+magick "temp.png" bmp3:"dialog.bmp"
 del "temp.png"
 move /y *.bmp ..\wix
